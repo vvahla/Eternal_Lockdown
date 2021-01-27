9119368b13de09d2ea4913077a95ea8cdd674800
vvahla <vahlaville@gmail.com>
2021-01-27 13:15:24 +0200

    Reloading

--- STG_game/Assets/scripts/GunScript.cs
+++ STG_game/Assets/scripts/GunScript.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
     public float fireRate = 15f;
     public float impactForce = 30f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
 
     public Camera fpsCam;
 
@@ -16,9 +21,34 @@ public class GunScript : MonoBehaviour
 
     public float nextTimeToFire = 0f;
 
-    // Update is called once per frame
+    public Animator animator;
+
+    public Text ammoCount;
+
+    private void Start()
+    {
+            currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
     void Update()
     {
+
+        ammoCount.text = currentAmmo.ToString() + " / " + maxAmmo.ToString();
+
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
 
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
@@ -26,11 +56,36 @@ public class GunScript : MonoBehaviour
             Shoot();
         }
 
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo)
+
+        {
+
+            StartCoroutine(Reload());
+
+        }
+
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 
     void Shoot()
     {
         muzzleFlash.Play();
+
+        currentAmmo--;
 
         RaycastHit hit;
 
